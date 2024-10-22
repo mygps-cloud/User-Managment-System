b56d3ec0f5da859752406aeba09a613af481f8ba
Dai <gtugushi67@gmail.com>
2024-10-22 16:50:15 +0400

    kakua

--- obj/Debug/net8.0/ipstatuschecker.AssemblyInfo.cs
+++ obj/Debug/net8.0/ipstatuschecker.AssemblyInfo.cs
@@ -13,11 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ipstatuschecker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-<<<<<<< HEAD
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ea399e691f90a1f10986ea6e2d751ec69fbd044")]
-=======
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f3fc729b17251a5c6ce9199a1508b9615d7a95a8")]
->>>>>>> 56106f589c8a88fe742d38ea515a30b2fc984e25
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b26ea9fbdd55a2c7d58eac82e6221cbb62df7ec")]
 [assembly: System.Reflection.AssemblyProductAttribute("ipstatuschecker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ipstatuschecker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
