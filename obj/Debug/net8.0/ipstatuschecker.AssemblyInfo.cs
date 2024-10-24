47d2548c2f74efd34c4e16b2256453416a81b949
Dai <gtugushi67@gmail.com>
2024-10-24 10:20:36 +0400

    robka make new upddate cscs styles

--- obj/Debug/net8.0/ipstatuschecker.AssemblyInfo.cs
+++ obj/Debug/net8.0/ipstatuschecker.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ipstatuschecker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d60edb627e1088fcee39f320866d62db5bbf549")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+31b0906cf5ca503326cd7fe47f37521a612572cb")]
 [assembly: System.Reflection.AssemblyProductAttribute("ipstatuschecker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ipstatuschecker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
