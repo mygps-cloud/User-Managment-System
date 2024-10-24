1d60edb627e1088fcee39f320866d62db5bbf549
neo <gtugushi67@gmail.com>
2024-10-24 07:59:53 +0400

    new test12

--- obj/Debug/net8.0/ipstatuschecker.AssemblyInfo.cs
+++ obj/Debug/net8.0/ipstatuschecker.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ipstatuschecker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ef515d0c41d611fc5ef1299e0c4128e0a42a05ca")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+528925f158804cf6c313f5bca529d2f45757bac8")]
 [assembly: System.Reflection.AssemblyProductAttribute("ipstatuschecker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ipstatuschecker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
