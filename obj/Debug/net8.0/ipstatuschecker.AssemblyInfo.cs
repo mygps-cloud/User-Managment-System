d1189ff21a79849cdc095ad5ed688f6dc1aa32e1
Dai <gtugushi67@gmail.com>
2024-10-30 16:30:15 +0400

    new style page users

--- obj/Debug/net8.0/ipstatuschecker.AssemblyInfo.cs
+++ obj/Debug/net8.0/ipstatuschecker.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ipstatuschecker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+176e978e00625e58103f5b6983fe948572ea08e2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c7cd82f35f57cacc9a030325a4e50e93bba535a")]
 [assembly: System.Reflection.AssemblyProductAttribute("ipstatuschecker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ipstatuschecker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
