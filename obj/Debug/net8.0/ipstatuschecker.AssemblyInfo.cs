40d844241ef7533280b88b1ecdfd999340d363c5
Dai <gtugushi67@gmail.com>
2024-10-31 17:41:29 +0400

    stash

--- obj/Debug/net8.0/ipstatuschecker.AssemblyInfo.cs
+++ obj/Debug/net8.0/ipstatuschecker.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ipstatuschecker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bad20b966c2c0f9af48df1cad4e0dfe8077833c9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a9e889710c8c22b0ba4ae0d4fb648e04d961bf03")]
 [assembly: System.Reflection.AssemblyProductAttribute("ipstatuschecker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ipstatuschecker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
