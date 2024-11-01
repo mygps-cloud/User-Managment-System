09771d04f2c6f2ef873ee1e79e21a83e906a0a96
Dai <gtugushi67@gmail.com>
2024-11-01 13:30:26 +0400

    add new style ccs

--- Background_Infrastructure/Services/PingLogService.cs
+++ Background_Infrastructure/Services/PingLogService.cs
@@ -23,11 +23,11 @@ public async Task<bool> addService(PingLogDtoReqvest entity)
 {
     if (entity == null) throw new ArgumentNullException(nameof(entity));
 
-    var existingLog = await context.PingLog.FirstOrDefaultAsync(pl => pl.UserId == entity.UserId);
-    var hasOnlineRecordForToday = existingLog?.OnlieTime.Any(time => time.Day == DateTime.Now.Day) ?? false;
-    var hasSufficientTimePassed = 
-    existingLog?.OnlieTime.Count > 0 &&(DateTime.Now - existingLog.OnlieTime.Last()).TotalSeconds >= 50;
-    var hasOfflineRecordForToday = existingLog?.OflineTime.Any(time => time.Day == DateTime.Now.Day) ?? false;
+var existingLog = await context.PingLog.FirstOrDefaultAsync(pl => pl.UserId == entity.UserId);
+var hasOnlineRecordForToday = existingLog?.OnlieTime?.Any(time => time.Day == DateTime.Now.Day) ?? false;
+var hasSufficientTimePassed = existingLog?.OnlieTime?.Count > 0 &&(DateTime.Now - existingLog.OnlieTime.Last()).
+Minutes >= 1;
+var hasOfflineRecordForToday = existingLog?.OflineTime?.Any(time => time.Day == DateTime.Now.Day) ?? false;
 
     try
     {
@@ -44,13 +44,15 @@ public async Task<bool> addService(PingLogDtoReqvest entity)
             
             if (!hasOnlineRecordForToday && hasSufficientTimePassed)
             {
-                existingLog.OnlieTime.Add(DateTime.Now);
+                existingLog?.OnlieTime?.Add(DateTime.Now);
             }
 
-            if (existingLog.OnlieTime.Count > 0 && !hasOfflineRecordForToday &&
-                (DateTime.Now - existingLog.OnlieTime.Last()).TotalSeconds >= 50)
+            if (existingLog?.OnlieTime?.Count > 0 && !hasOfflineRecordForToday &&
+                (DateTime.Now - existingLog.OnlieTime.Last()).Minutes >= 1
+                &&entity?.OflineTime?.Count>0)
             {
-                existingLog.OflineTime.Add(DateTime.Now);
+                existingLog?.OflineTime?.Add(DateTime.Now.AddMinutes(-1));
+             
             }
 
             return await pingLogRepository.Save();
@@ -58,7 +60,7 @@ public async Task<bool> addService(PingLogDtoReqvest entity)
         else
         {
           
-            if (entity.OnlieTime.Count > 0)
+            if (entity.OnlieTime?.Count > 0)
             {
                 await pingLogRepository.Create(pingLog);
                 return true;
