0504f4d45c454c6d023d0a4375b8ffe5b7a884f2
Dai <gtugushi67@gmail.com>
2024-11-12 16:59:17 +0400

    update service names

--- Background_Infrastructure/Services/TimeControlServices/CheckInOutserviceTimeControlService.cs
+++ Background_Infrastructure/Services/TimeControlServices/CheckInOutserviceTimeControlService.cs
@@ -13,12 +13,12 @@ namespace Ipstatuschecker.Background_Infrastructure.Services.TimeControlServices
                 HasOnlineRecordForToday = entity?.OnlineTime?.Any(time => time.Day == DateTime.Now.Day) ?? false,
                 
                 HasSufficientTimePassed = entity?.OnlineTime?.Count > 0 && 
-                                          entity.OnlineTime?.Any(time => time.Day == DateTime.Now.Day) == true,
+                entity.OnlineTime?.Any(time => time.Day == DateTime.Now.Day) == true,
                 
                 HasOfflineRecordForToday = entity?.OflineTime?.Any(time => time.Day == DateTime.Now.Day) ?? false,
                 
                 LastTimeIn = entity?.OnlineTime != null && entity.OnlineTime.Any() && 
-                             !Status && (DateTime.Now - entity.OnlineTime.Last()).Hours >= 3
+                !Status && (DateTime.Now - entity.OnlineTime.Last()).Hours >= 3
             };
 
             return Task.FromResult(Result);
--- obj/Debug/net8.0/ipstatuschecker.AssemblyInfo.cs
+++ obj/Debug/net8.0/ipstatuschecker.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ipstatuschecker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ad2a4a757ccd62bc2f78004b2596a310130d12a9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+acd38493526bd863bd9df55f27cf6cfdc3fee174")]
 [assembly: System.Reflection.AssemblyProductAttribute("ipstatuschecker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ipstatuschecker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
